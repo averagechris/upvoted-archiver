bc0a2c9ede7398493c1f2279ba8bc8d590fb1e25
Chris Cummings <chris@thesogu.com>
2022-05-31 22:35:56 -0500

    Add basic async iterator over upvoted posts

--- upvoted_archiver/test/test.rs
+++ upvoted_archiver/test/test.rs
@@ -1,5 +1,26 @@
+use futures_util::pin_mut;
+use futures_util::stream::StreamExt;
+use std::env;
+use upvoted_archiver::{Config, RedditCredentials, Upvotes};
+
 #[cfg(test)]
-#[test]
-fn test_me_i_am_working() {
-    assert_eq!(2 + 2, 4);
+#[tokio::test]
+async fn test_me() {
+    let config = Config::default();
+    let mut upvotes = Upvotes {
+        config: &config,
+        credentials: RedditCredentials::new(
+            &env::var("REDDIT_USERNAME").expect("SHIT?"),
+            &env::var("REDDIT_PASSWORD").expect("SHIT?"),
+        ),
+        me: None,
+    };
+
+    let stream = upvotes.as_stream();
+    pin_mut!(stream);
+    let mut trunc_stream = stream.take(15);
+
+    while let Some(thing) = trunc_stream.next().await {
+        dbg!(thing).unwrap();
+    }
 }
